5fd448e7bbb9f8875e3fbac6553263a8a3ba18ac
Donal Spring <donalspring@gmail.com>
2016-03-16 09:43:59 +0000

    FIX - removal of the route

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/'));
-app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-    .get(errors[404]);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
